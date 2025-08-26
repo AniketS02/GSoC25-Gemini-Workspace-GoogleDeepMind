60bb29eda61ed7d5e49956bedd801fe6339baf55
AniketS02 <saxena.aniket02@gmail.com>
2025-08-26 22:41:27 +0530

    Updated Scripts

--- scripts/update-postman-collection.js
+++ scripts/update-postman-collection.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const postmanApiKey = process.env.POSTMAN_API_KEY;
 const workspaceId = process.env.POSTMAN_WORKSPACE_ID;
-const collectionId = process.env.TEXT_GENERATION_COLLECTION_ID;
+const collectionId = process.env.GEMINI_API_COLLECTION_ID;
 const apiSpecPath = path.join(__dirname, '../api-specs/latest-spec.json');
 
 const apiSpec = JSON.parse(fs.readFileSync(apiSpecPath, 'utf8'));
